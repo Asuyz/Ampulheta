0f96b63a0382c0263a4af6575b954bfa4e024a52
Asuyz <aemygdioferreira@gmail.com>
2025-09-28 20:43:02 -0300

    Criação de uma ampulheta simples, utilizando python (time e tkinter)

---
+++ MinhaAmpulheta.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import time
+
+
+print("Seja bem vindo a sua ampulheta ")
+print("""
+  .-.
+ (   )
+  ) (
+ (   )
+  `-'
+""")
+print("Digite o tempo que gostaria em minutos | (Exemplo:120 minutos)")
+
+tempo=int(input())
+
+segundos = tempo *60
+
+i = 0
+
+while i < tempo:
+    i = i+1
+    time.sleep(60)
+    print (i)
+  
+def abrir_janela():
+    janela = tk.Tk()
+    janela.title("Tempo Esgotado")
+    janela.geometry("300x150")
+
+    label = tk.Label(janela, text="⏳ O tempo acabou!", font=("Arial", 14))
+    label.pack(pady=30)
+
+    botao = tk.Button(janela, text="OK", command=janela.destroy)
+    botao.pack()
+
+    janela.mainloop()
+
+abrir_janela()
