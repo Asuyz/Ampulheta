7781ffe0a5510fe6eadb46785d5f7302f3cd1903
Asuyz <aemygdioferreira@gmail.com>
2025-09-30 09:27:08 -0300

    Lógica salva no arquivo como backup, futura implentação de janelas em breve

--- MinhaAmpulheta.py
+++ MinhaAmpulheta.py
@@ -1,44 +1,68 @@
 import tkinter as tk
 import time
 
-while True:
-    try:
-        print('Seja bem-vindo(a) a sua ampulheta. | 1- Continuar | 2- Sair')
-        escolha= int(input())
-        
-        if escolha == 1:
-            print("""
-            +====+   
-            |(::)|
-            | )( |
-            |(..)|
-            +====+
-            """)
-            print("""Obrigado por ter continuado, para comerçarmos digite o tempo desejado em minutos | 
-            Exemplo: 120 minutos ( 2Horas )""")
-            tempo=int(input())
 
-            segundos = tempo * 60
-            i=0
-            while i < tempo:
-                i = i + 1
-                time.sleep(60)
-                print(i)
-                print ('Deseja continuar? | 1 - Sim | 2 - Não')
-                continuar = int(input())
-                try:
-                    if continuar == 1:
-                        True
-                    else:
-                         False
-                except:
-                    ValueError('Escolha incorreta. Escolha novamente.')
-    except:
-        escolha == 2
-        False
-    finally:
-        ValueError('Escolha incorreta. Escolha novamente.')
-        True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Backup da Lógica
+
+#while True:
+#    try:
+#        print('Seja bem-vindo(a) a sua ampulheta. | 1- Continuar | 2- Sair')
+#        escolha= int(input())
+#        
+#        if escolha == 1:
+#            print("""
+#            +====+   
+#            |(::)|
+#            | )( |
+#            |(..)|
+#            +====+
+#            """)
+#            print("""Obrigado por ter continuado, para comerçarmos digite o tempo desejado em minutos | 
+#            Exemplo: 120 minutos ( 2Horas )""")
+#            tempo=int(input())
+
+#            segundos = tempo * 60
+#            i = 0
+#            while i < tempo:
+#                i = i + 1
+#                time.sleep(60)
+#                print(i)
+#                print ('Deseja continuar? | 1 - Sim | 2 - Não')
+#                continuar = int(input())
+#                try:
+#                    if continuar == 1:
+#                        True
+#                    else:
+#                         False
+#                except:
+#                    ValueError('Escolha incorreta. Escolha novamente.')
+#    except:
+#        escolha == 2
+#        False
+#    finally:
+#        ValueError('Escolha incorreta. Escolha novamente.')
+#        True
     
 
   
